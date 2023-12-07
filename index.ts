e01ce994ca6ac0f8e26189d217064c88079b659d
Dian Ramdhani Lubis <dian.lubis@prosa.ai>
2023-12-07 11:04:57 +0700

    combine products

--- index.ts
+++ index.ts
@@ -3,36 +3,32 @@ import puppeteer from 'puppeteer'
 import { CronJob } from 'cron'
 import { addProductToCart, checkOut, login, prepareCheckout } from './core'
 
-const {
-  URL,
-  EMAIL,
-  PASSWORD,
-  TIME_PAYMENT,
-  URL_CART,
-  URL_PAYMENT,
-  URL_PRODUCT,
-} = process.env
+const { URL, PASSWORD, TIME_PAYMENT, URL_PAYMENT, URL_CART } = process.env
+const emails = (process.env['EMAILS'] ?? '').split(' ')
+const urlProducts = (process.env['URL_PRODUCTS'] ?? '').split(' ')
 const isProd = process.env['ENV'] === 'prod'
 const jobLogin = new CronJob(process.env['TIME_LOGIN'] ?? '', main)
 isProd ? jobLogin.start() : main()
 
-async function main() {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  })
-  const [page] = await browser.pages()
-  const jobPayment = new CronJob(TIME_PAYMENT ?? '', () => {
-    checkOut(page, URL_PAYMENT ?? '')
-  })
+function main() {
+  urlProducts.forEach(async (urlProduct, index) => {
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+    })
+    const [page] = await browser.pages()
+    const jobPayment = new CronJob(TIME_PAYMENT ?? '', () => {
+      checkOut(page, URL_PAYMENT ?? '')
+    })
 
-  try {
-    await page.goto(URL ?? '')
-    await login(page, EMAIL ?? '', PASSWORD ?? '')
-    await addProductToCart(page, URL_PRODUCT ?? '')
-    await prepareCheckout(page, URL_CART ?? '')
-    isProd ? jobPayment.start() : checkOut(page, URL_PAYMENT ?? '')
-  } catch (error) {
-    console.warn('ada error gatau apa', error)
-  }
+    try {
+      await page.goto(URL ?? '')
+      await login(page, emails[index], PASSWORD ?? '')
+      await addProductToCart(page, urlProduct)
+      await prepareCheckout(page, URL_CART ?? '')
+      isProd ? jobPayment.start() : checkOut(page, URL_PAYMENT ?? '')
+    } catch (error) {
+      console.warn('ada error gatau apa', error)
+    }
+  })
 }
