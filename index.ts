25c49719a22bbc752f8327ff63d38c0104d08e98
Dian Ramdhani Lubis <dian.ramdhani@gmail.com>
2023-12-07 08:09:07 +0700

    implement addProductToCart

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config'
 import puppeteer from 'puppeteer'
 import { CronJob } from 'cron'
-import { checkOut, login, prepareCheckout } from './core'
+import { addProductToCart, checkOut, login, prepareCheckout } from './core'
 
-const { URL, EMAIL, PASSWORD, TIME_PAYMENT, URL_CART, URL_PAYMENT } =
-  process.env
+const {
+  URL,
+  EMAIL,
+  PASSWORD,
+  TIME_PAYMENT,
+  URL_CART,
+  URL_PAYMENT,
+  URL_PRODUCT,
+} = process.env
 const isProd = process.env['ENV'] === 'prod'
 const jobLogin = new CronJob(process.env['TIME_LOGIN'] ?? '', main)
 isProd ? jobLogin.start() : main()
@@ -22,6 +29,7 @@ async function main() {
   try {
     await page.goto(URL ?? '')
     await login(page, EMAIL ?? '', PASSWORD ?? '')
+    await addProductToCart(page, URL_PRODUCT ?? '')
     await prepareCheckout(page, URL_CART ?? '')
     isProd ? jobPayment.start() : checkOut(page, URL_PAYMENT ?? '')
   } catch (error) {
