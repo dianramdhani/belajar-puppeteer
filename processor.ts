0c7bf81084aad34f4e84a32adbbe448ba005ca94
Dian Ramdhani Lubis <dian.lubis@prosa.ai>
2023-12-10 08:11:56 +0700

    minor fix

--- processor.ts
+++ processor.ts
@@ -163,65 +163,73 @@ export default class Processor {
       await this.page?.evaluate(
         async (urlQuery: string, headers: Record<string, string>) => {
           try {
-            const processCheckout = await fetch(urlQuery, {
-              method: 'POST',
-              body: JSON.stringify([
-                {
-                  operationName: 'processCheckout',
-                  variables: {},
-                  query:
-                    'query processCheckout {\n  processCheckout {\n    meta {\n      message\n      error\n      code\n    }\n    result\n  }\n}\n',
-                },
-              ]),
-              headers,
-            })
-            const addPreBook = await fetch(urlQuery, {
-              method: 'POST',
-              body: JSON.stringify([
-                {
-                  operationName: 'addPreBook',
-                  variables: {
-                    params: {
-                      isRewardPoint: false,
-                      addressID: 595706,
-                      shippingID: 4,
-                      shippingName: 'J&T',
-                      shippingDuration: 'Estimasi pengiriman 2-3 Hari',
-                    },
+            while (true) {
+              const processCheckout = await fetch(urlQuery, {
+                method: 'POST',
+                body: JSON.stringify([
+                  {
+                    operationName: 'processCheckout',
+                    variables: {},
+                    query:
+                      'query processCheckout {\n  processCheckout {\n    meta {\n      message\n      error\n      code\n    }\n    result\n  }\n}\n',
                   },
-                  query:
-                    'mutation addPreBook($params: PreBookRequest!) {\n  addPreBook(params: $params) {\n    meta {\n      message\n      error\n      code\n    }\n    result {\n      status\n      orderID\n      analytic {\n        affiliation\n        coupon\n        currency\n        transaction_id\n        shipping\n        insurance\n        value\n        partial_reward\n        coupon_discount\n        shipping_discount\n        location\n        quantity\n        items {\n          item_id\n          item_name\n          affiliation\n          coupon\n          currency\n          discount\n          index\n          item_brand\n          item_category\n          item_category2\n          item_category3\n          item_category4\n          item_category5\n          item_list_id\n          item_list_name\n          item_variant\n          price\n          quantity\n        }\n        content_id\n        content_type\n        contents {\n          id\n          quantity\n        }\n        description\n        category_id\n        category_name\n        brand_id\n        brand_name\n        sub_brand_id\n        sub_brand_name\n        order_id\n        order_date\n        total_trx\n        shipping_fee\n        insurance_fee\n        tax\n        discount\n        partial_mw_reward\n        shipping_method\n        payment_method\n        is_dropship\n        voucher_code\n        products\n      }\n    }\n  }\n}\n',
-                },
-              ]),
-              headers,
-            })
-            const addOrder = await fetch(urlQuery, {
-              method: 'POST',
-              body: JSON.stringify([
-                {
-                  operationName: 'addOrder',
-                  variables: {
-                    params: {
-                      paymentID: 57,
-                      paymentCode: 'VABCA',
-                      paymentName: 'BCA Virtual Account',
-                      paymentParentCode: 'VirtualAccount',
+                ]),
+                headers,
+              })
+              const addPreBook = await fetch(urlQuery, {
+                method: 'POST',
+                body: JSON.stringify([
+                  {
+                    operationName: 'addPreBook',
+                    variables: {
+                      params: {
+                        isRewardPoint: false,
+                        addressID: 595706,
+                        shippingID: 4,
+                        shippingName: 'J&T',
+                        shippingDuration: 'Estimasi pengiriman 2-3 Hari',
+                      },
                     },
+                    query:
+                      'mutation addPreBook($params: PreBookRequest!) {\n  addPreBook(params: $params) {\n    meta {\n      message\n      error\n      code\n    }\n    result {\n      status\n      orderID\n      analytic {\n        affiliation\n        coupon\n        currency\n        transaction_id\n        shipping\n        insurance\n        value\n        partial_reward\n        coupon_discount\n        shipping_discount\n        location\n        quantity\n        items {\n          item_id\n          item_name\n          affiliation\n          coupon\n          currency\n          discount\n          index\n          item_brand\n          item_category\n          item_category2\n          item_category3\n          item_category4\n          item_category5\n          item_list_id\n          item_list_name\n          item_variant\n          price\n          quantity\n        }\n        content_id\n        content_type\n        contents {\n          id\n          quantity\n        }\n        description\n        category_id\n        category_name\n        brand_id\n        brand_name\n        sub_brand_id\n        sub_brand_name\n        order_id\n        order_date\n        total_trx\n        shipping_fee\n        insurance_fee\n        tax\n        discount\n        partial_mw_reward\n        shipping_method\n        payment_method\n        is_dropship\n        voucher_code\n        products\n      }\n    }\n  }\n}\n',
                   },
-                  query:
-                    'mutation addOrder($params: AddOrderRequest!) {\n  addOrder(params: $params) {\n    meta {\n      error\n      code\n      message\n    }\n    result {\n      payment {\n        status\n        orderId\n        redirectUrl\n      }\n      analytic {\n        affiliation\n        coupon\n        currency\n        transaction_id\n        transaction_code\n        shipping\n        insurance\n        value\n        partial_reward\n        coupon_discount\n        shipping_discount\n        location\n        quantity\n        items {\n          item_id\n          item_name\n          affiliation\n          currency\n          discount\n          index\n          item_brand\n          item_category\n          item_category2\n          item_category3\n          item_category4\n          item_category5\n          item_list_id\n          item_list_name\n          item_variant\n          price\n          quantity\n        }\n        content_id\n        content_type\n        contents {\n          id\n          quantity\n        }\n        description\n        category_id\n        category_name\n        brand_id\n        brand_name\n        sub_brand_id\n        sub_brand_name\n        order_id\n        order_date\n        total_trx\n        shipping_fee\n        insurance_fee\n        tax\n        discount\n        partial_mw_reward\n        shipping_method\n        payment_method\n        is_dropship\n        voucher_code\n        products\n        total_price\n        gender\n        db\n        user_id\n        fb_login_id\n        ip_override\n        user_data {\n          email_address\n          phone_number\n          client_ip_address\n          address {\n            first_name\n            last_name\n            city\n            region\n            postal_code\n            country\n          }\n        }\n      }\n    }\n  }\n}\n',
-                },
-              ]),
-              headers,
-            })
+                ]),
+                headers,
+              })
+              const addOrder = await fetch(urlQuery, {
+                method: 'POST',
+                body: JSON.stringify([
+                  {
+                    operationName: 'addOrder',
+                    variables: {
+                      params: {
+                        paymentID: 57,
+                        paymentCode: 'VABCA',
+                        paymentName: 'BCA Virtual Account',
+                        paymentParentCode: 'VirtualAccount',
+                      },
+                    },
+                    query:
+                      'mutation addOrder($params: AddOrderRequest!) {\n  addOrder(params: $params) {\n    meta {\n      error\n      code\n      message\n    }\n    result {\n      payment {\n        status\n        orderId\n        redirectUrl\n      }\n      analytic {\n        affiliation\n        coupon\n        currency\n        transaction_id\n        transaction_code\n        shipping\n        insurance\n        value\n        partial_reward\n        coupon_discount\n        shipping_discount\n        location\n        quantity\n        items {\n          item_id\n          item_name\n          affiliation\n          currency\n          discount\n          index\n          item_brand\n          item_category\n          item_category2\n          item_category3\n          item_category4\n          item_category5\n          item_list_id\n          item_list_name\n          item_variant\n          price\n          quantity\n        }\n        content_id\n        content_type\n        contents {\n          id\n          quantity\n        }\n        description\n        category_id\n        category_name\n        brand_id\n        brand_name\n        sub_brand_id\n        sub_brand_name\n        order_id\n        order_date\n        total_trx\n        shipping_fee\n        insurance_fee\n        tax\n        discount\n        partial_mw_reward\n        shipping_method\n        payment_method\n        is_dropship\n        voucher_code\n        products\n        total_price\n        gender\n        db\n        user_id\n        fb_login_id\n        ip_override\n        user_data {\n          email_address\n          phone_number\n          client_ip_address\n          address {\n            first_name\n            last_name\n            city\n            region\n            postal_code\n            country\n          }\n        }\n      }\n    }\n  }\n}\n',
+                  },
+                ]),
+                headers,
+              })
 
-            console.log(
-              await Promise.all([
+              const responses: any[] = await Promise.all([
                 processCheckout.json(),
                 addPreBook.json(),
                 addOrder.json(),
               ])
-            )
+
+              console.log(responses)
+
+              if (!!responses[2].data.addOrder.meta.error) {
+                await new Promise((resolve) => setTimeout(resolve, 3000))
+              } else {
+                break
+              }
+            }
           } catch (error) {
             console.error(error)
           }
