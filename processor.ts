fc93a2ae185cbdd2d162f621da98fd652475f354
Dian Ramdhani Lubis <dian.lubis@prosa.ai>
2023-12-12 11:13:28 +0700

    minor fix

--- processor.ts
+++ processor.ts
@@ -180,6 +180,7 @@ export default class Processor {
           try {
             const responseData = await response.json()
             this.headers = response.request().headers()
+            console.info('headers updated')
           } catch (error) {}
         }
       })
